1834431ea56eb7901d60dd720e1dcba2e78bb9d9
teerawat.th <teerawat.th@baac.or.th>
2018-07-13 14:48:26 +0700

    [Created]งก์ชัน NewBoard

---
+++ bingo.go
@@ -0,0 +1,34 @@
+package bingo
+
+type Player struct {
+	Name string
+}
+
+type State struct {
+	Symbol string
+	Number int
+}
+
+type Size struct {
+	X int
+	Y int
+}
+
+type Board struct {
+	Slots  [][]State
+	Player Player
+	Size   Size
+}
+
+func NewBoard(player Player) Board {
+	slots := make([][]State, 5)
+	for index := 0; index < 5; index++ {
+		slots[index] = make([]State, 5)
+	}
+
+	return Board{
+		Slots:  slots,
+		Player: player,
+		Size:   Size{X: 5, Y: 5},
+	}
+}
