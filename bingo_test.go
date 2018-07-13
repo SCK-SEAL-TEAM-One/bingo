b42b1261795e738e8de3e3b9be4628764c13a02c
teerawat.th <teerawat.th@baac.or.th>
2018-07-13 14:49:01 +0700

    [Created] : ฟังก์ชัน test NewBoard

---
+++ bingo_test.go
@@ -0,0 +1,19 @@
+package bingo
+
+import "testing"
+
+func Test_NewBoard_Input_Player_Aey_Should_Be_Board_Player_Aey(t *testing.T) {
+	player := Player{Name: "Aey"}
+
+	expected := Board{
+		Player: Player{Name: "Aey"},
+		Size:   Size{X: 5, Y: 5},
+	}
+
+	actual := NewBoard(player)
+
+	if actual.Player != expected.Player || expected.Size != actual.Size {
+		t.Errorf("expected %v but got %v ", actual, expected)
+	}
+
+}
